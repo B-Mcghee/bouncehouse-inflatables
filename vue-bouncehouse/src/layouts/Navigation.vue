aa6b4dba5b0740c28340e34cf4360bdc688c7052
Brandon McGhee <bmcghee.dev@gmail.com>
2021-07-05 20:49:37 -0400

    added layouts for navigation and toolbar

---
+++ vue-bouncehouse/src/layouts/Navigation.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+    <div>
+      <nav class="text-white">
+        <div class="bg-black grid grid-cols-12">
+          <ul class="grid grid-cols-12 col-span-12 h-12 items-center">
+            <li class="col-start-0 col-end-1 text-2xl">
+              <router-link to="/">Bounce</router-link>
+            </li>
+            <li class="col-start-11 col-span-1 text-2xl">&#9776;</li>
+          </ul>
+        </div>
+        <Toolbar />
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+import Toolbar from "./Toolbar.vue";
+export default {
+  name: "Navbar",
+  components: {
+    Toolbar,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ vue-bouncehouse/src/layouts/Toolbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="bg-black bg-opacity-70 text-white">
+    <ul class="grid grid-cols-12">
+      <li class="col-span-3 gap-1 border-blue-900 border-2">
+        <router-link to="/products">See All</router-link>
+      </li>
+      <li @click="checkout()" class="col-span-6 gap-1 border-blue-900 border-2">
+        Create Reservation
+      </li>
+      <li class="col-span-3 gap-1 border-blue-900 border-2">
+        <router-link to="about">FAQ</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { checkout } from "../mixins/Mixins";
+export default {
+  name: "Toolbar",
+  mixins: [checkout],
+};
+</script>
+
+<style>
+</style>
